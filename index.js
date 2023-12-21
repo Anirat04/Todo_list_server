9ce0cf9294325cf401cf3386e64d4d79dd693ae3
Tanbir Haque <tanbirhaque53@gmail.com>
2023-12-21 23:38:18 +0600

    Server side api created

--- index.js
+++ index.js
@@ -21,28 +21,128 @@ app.listen(port, () => {
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrhwfvt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const todoListCollection = client.db("todoDB").collection("todoLIST");
+        const ongoingListCollection = client.db("todoDB").collection("ongoingList");
+        const completedListCollection = client.db("todoDB").collection("completedList");
+
+        app.get('/allTodoList', async (req, res) => {
+            const result = await todoListCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/todoList', async (req, res) => {
+            const email = req.query.email;
+            const query = { userEmail: email };
+            const result = await todoListCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/todoList', async (req, res) => {
+            const TodoItem = req.body;
+            const result = await todoListCollection.insertOne(TodoItem)
+            res.send(result)
+        })
+        app.delete('/todoList/:id', async (req, res) => {
+            const taskID = req.params.id
+            const query = { _id: new ObjectId(taskID) }
+            const result = await todoListCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.delete('/todoList2/:id', async (req, res) => {
+            const taskID = req.params.id
+            const query = { _id: taskID }
+            const result = await todoListCollection.deleteOne(query)
+            res.send(result)
+        })
+        // app.patch('/todoList3/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const filter = { _id: new ObjectId(id) }
+        //     const updateProperty = req.body;
+        //     const result = await todoListCollection.updateOne(filter, { $set: updateProperty })
+        //     res.send(result)
+        // })
+        // app.patch('/todoList4/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const filter = { _id: id }
+        //     const updateProperty = req.body;
+        //     const result = await todoListCollection.updateOne(filter, { $set: updateProperty })
+        //     res.send(result)
+        // })
+
+
+
+
+
+        app.get('/ongoingList', async (req, res) => {
+            const email = req.query.email;
+            const query = { userEmail: email };
+            const result = await ongoingListCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/ongoingList', async (req, res) => {
+            const TodoItem = req.body;
+            const result = await ongoingListCollection.insertOne(TodoItem)
+            res.send(result)
+        })
+
+        app.delete('/ongoingList/:id', async (req, res) => {
+            const ongoingID = req.params.id
+            const query = { _id: ongoingID }
+            const result = await ongoingListCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
+        app.get('/completedList', async (req, res) => {
+            const email = req.query.email;
+            const query = { userEmail: email };
+            const result = await completedListCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/completedList', async (req, res) => {
+            const completedItem = req.body;
+            const result = await completedListCollection.insertOne(completedItem)
+            res.send(result)
+        })
+
+        app.delete('/completedList/:id', async (req, res) => {
+            const completedID = req.params.id
+            const query = { _id: completedID }
+            const result = await completedListCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
